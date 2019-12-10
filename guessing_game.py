4e148bb31005641476abba655a38cc2fed406e8d
Fabiana Masini <masinifabiana@gmail.com>
2019-12-10 16:59:54 -0300

    implementing first game conditionals

--- guessing_game.py
+++ guessing_game.py
@@ -3,3 +3,11 @@ print("Welcome to guessing game!")
 print("*************************")
 
 secret_number = 42
+guess_str = input("Enter your number: ")
+guess = int(guess_str)
+
+if guess == secret_number:
+    print("You're right!")
+else:
+    print("You missed :(")
+print("End of the game :)")
