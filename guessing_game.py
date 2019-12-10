a2f4f6935e0554bed2ebe4b112936db03faba18a
Fabiana Masini <masinifabiana@gmail.com>
2019-12-10 17:32:24 -0300

    implementing more conditionals (check if guess is greater or less than the secret number) and counting rounds functionality with while loop

--- guessing_game.py
+++ guessing_game.py
@@ -3,11 +3,26 @@ print("Welcome to guessing game!")
 print("*************************")
 
 secret_number = 42
-guess_str = input("Enter your number: ")
-guess = int(guess_str)
+number_of_tries = 5
+game_round = 1
 
-if guess == secret_number:
-    print("You're right!")
-else:
-    print("You missed :(")
+while game_round <= number_of_tries:
+    print("Round {} of {}".format(game_round, number_of_tries))
+    guess_str = input("Enter your number: ")
+    guess = int(guess_str)
+
+    right_answer = guess == secret_number
+    greater_than = guess > secret_number
+
+    if right_answer:
+        print("You're right!")
+        break
+    elif greater_than:
+        print("You missed :( Your guess is greater than the secret number!")
+        game_round = game_round + 1
+    else:
+        print("You missed :( Your guess is less than the secret number!")
+        game_round = game_round + 1
+
+print("\n")
 print("End of the game :)")
