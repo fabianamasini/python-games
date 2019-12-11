38b4537f51e052790becfea4022c4b3e852b4320
Fabiana Masini <masinifabiana@gmail.com>
2019-12-11 01:21:06 -0300

    punctuation rules added

--- guessing_game.py
+++ guessing_game.py
@@ -5,8 +5,27 @@ print("Welcome to guessing game!")
 print("*************************")
 
 secret_number = round(rdm.randrange(1, 101))
-number_of_tries = 5
+number_of_tries = 0
 game_round = 1
+points = 1000
+lost_points = 0
+
+print("What's the difficulty level?")
+print("[1] Easy [2] Medium [3] Hard")
+
+level = int(input())
+
+if level == 1:
+    number_of_tries = 20
+    print(f"You have {number_of_tries} tries. Good luck!")
+elif level == 2:
+    number_of_tries = 10
+    print(f"You have {number_of_tries} tries. Good luck!")
+elif level == 3:
+    number_of_tries = 5
+    print(f"You have {number_of_tries} tries. Good luck!")
+else:
+    print("Invalid level number.")
 
 for game_round in range(1, number_of_tries + 1):
     print(f"Round {game_round} of {number_of_tries}")
@@ -25,11 +44,16 @@ for game_round in range(1, number_of_tries + 1):
         break
     elif greater_than:
         print("You missed :( Your guess is greater than the secret number!")
+        lost_points = guess - secret_number
+        points = points - lost_points
         continue
     else:
         print("You missed :( Your guess is less than the secret number!")
+        lost_points = secret_number - guess
+        points = points - lost_points
         continue
 
 print("\n")
 print("End of the game :)")
 print(f"The secret number was {secret_number}")
+print(f"Your final punctuation: {points}")
