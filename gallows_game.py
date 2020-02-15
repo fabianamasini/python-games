4c5023e1485fc757c37975a6626193d1eefe96d2
Fabiana Masini <masinifabiana@gmail.com>
2020-02-15 10:57:49 -0300

    first features from gallows game and configuration files.

--- gallows_game.py
+++ gallows_game.py
@@ -3,6 +3,21 @@ def play():
     print("Welcome to gallows game!")
     print("************************")
 
+    secret_word = "banana"
+    right_answer = False
+    hanged = False
+
+    while not right_answer and not hanged:
+        print("Which letter?")
+        guess = input().upper().strip()
+        formatted_secret_word = secret_word.upper().strip()
+        index = 0
+
+        for letter in formatted_secret_word:
+            if guess == letter:
+                print(f"Found letter {guess} in position {index}")
+            index = index + 1
+
 
 if __name__ == "__main__":
     play()
