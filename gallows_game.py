d5030855a17417de40e0030ad895591e3780969d
Fabiana Masini <masinifabiana@gmail.com>
2020-02-19 13:14:12 -0300

    adding list data structure so the user knows the number of letters inside the secret word. also, at each right guess, the list is modified and shows the right letters in their respective indexes.

--- gallows_game.py
+++ gallows_game.py
@@ -2,10 +2,20 @@ def play():
     print("************************")
     print("Welcome to gallows game!")
     print("************************")
+    print("Here's your secret word:")
 
     secret_word = "banana"
+    right_letters = []
     right_answer = False
     hanged = False
+
+    def right_letters_list():
+        for _ in secret_word:
+            right_letters.append("_")
+        return right_letters
+
+    right_letters = right_letters_list()
+    print(right_letters)
 
     while not right_answer and not hanged:
         print("Which letter?")
@@ -15,8 +25,10 @@ def play():
 
         for letter in formatted_secret_word:
             if guess == letter:
-                print(f"Found letter {guess} in position {index}")
+                right_letters[index] = guess
+                # print(f"Found letter {guess} in position {index}")
             index = index + 1
+        print(right_letters)
 
 
 if __name__ == "__main__":
